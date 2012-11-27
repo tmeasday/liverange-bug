2a364ad8d3b8c3a14ee177cb58d964722f93c71f
Tom Coleman <tom@thesnail.org>
2012-11-27 17:43:06 +1100

    First attempt to reproduce

---
+++ liverange-bug.js
@@ -0,0 +1,31 @@
+var Posts = new Meteor.Collection('posts');
+
+if (Meteor.isClient) {
+  Template.hello.post = function() {
+    return Posts.findOne();
+  }
+  
+  Template.hello.users = function() {
+    return [{_id: 'a', userName: 'tom'}, {_id: 'b', userName: '2'}];
+  }
+  
+  Template.hello.isSelected = function() {
+    var post = Posts.findOne();
+    return this._id === post.userId;
+  }
+  
+  Meteor.startup(function() {
+    Meteor.setInterval(function() {
+      Posts.update({}, {$inc: {score: 1}});
+    }, 1000);
+  })
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    if (Posts.find().count() === 0) {
+      console.log('inserting');
+      Posts.insert({score: 0});
+    }
+  });
+}
